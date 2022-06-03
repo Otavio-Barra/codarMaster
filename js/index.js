9c52753b77b9d0d16e93f3653a9c050acccc6e74
Otavio-Barra <otaviombarra@gmail.com>
2022-06-03 16:25:17 -0300

    projeto finalizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-var saudacao = prompt("seja bem vindo!\nPor favor informe seu nome para colocar no registro de acesso")
+const horaAtual = new Date().getHours()
 
-var agradecer = alert("Obrigado " + saudacao + "\nfico feliz pelo acesso")
+if (horaAtual <= 12) {
+    var saudacao = ("bom dia")
+} else if (horaAtual <= 18) {
+    var saudacao = ("boa tarde")
+} else var saudacao = ("boa noite")
+
+
+
+var ola = prompt(saudacao + " seja bem vindo!\nPor favor informe seu nome para colocar no registro de acesso")
+
+var agradecer = alert("Obrigado " + ola + "\nfico feliz pelo acesso")
